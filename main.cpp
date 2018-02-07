74676f3687d81d9c37a4eda4d86d98b7b9a5b1a6
thson <thson@humaxdigital.com>
2018-02-07 18:14:43 +0700

    hehe

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <queue>
 using namespace std;
 
 // (To use qsort) Check fisrt string is greater than second string or NOT
@@ -25,12 +26,12 @@ int min_pair(vector<pair<int, string>>, pair<int, string>&);
 
 int main()
 {
-	string inputFileName;						// input file from input
-	string outputfileName;						// output file from input
-	unsigned int RAM_SIZE;						// RAM_SIZE from input
-	string line;								// store 1 line of a file
-	int buffer_size = 0;						// store size (Byte) of a temporary file, it should be smalled than RAM_SIZE
-	string filename;							// store a filename
+	string inputFileName;                                              // input file name
+	string outputfileName;                                             // output file name
+	unsigned int RAM_SIZE;                                             // RAM_SIZE (MB) from input
+	string line;                                                       // store 1 line of a file
+	int buffer_size = 0;                                               // store size (Byte) of a temporary file, it should be smalled than RAM_SIZE
+	string filename;                                                   // store filename
 
 	// Input
 	cout << "Input file: ";
@@ -55,85 +56,72 @@ int main()
 		return 0;
 	}
 
-	// 1. Put temp_data to temporary files: text_temp_1.txt, text_temp_2.txt, ...
-	// Afer this step, this program creates some temporary files whose size can fit RAM_SIZE
-	ofstream temp_outputStream;			// Temporary output file
+	//   *** 1. Reading data that fit RAM_SIZE to sort using qsort. 
+	//          Then put temp_data to temporary files: text_temp_1.txt, text_temp_2.txt, ...
+	ofstream temp_outputStream;                                        // Temporary output file
 	filename = "text_temp_" + to_string(1) + ".txt";
 	temp_outputStream.open(filename);
-	int number_temp_file = 1;					// Number of temporary files
-	int is_first_line = 1;						// Check one line is first line of inputFile or NOT
+	int number_temp_file = 1;                                          // Number of temporary files
 
+	vector<string> temp_data;                                          // Data that fit RAM_SIZE
 	while (!inputStream.eof())
 	{
 		getline(inputStream, line);
 		if (buffer_size < RAM_SIZE * 1024 * 1024 - line.size())
 		{
-			// get line
-			if (!is_first_line)
-				temp_outputStream << endl;
-			temp_outputStream << line ;
 			buffer_size += line.size();
+			temp_data.push_back(line);
 		}
 		else
 		{
+			// sort, then store into each temporary file (not the last)
+			qsort(&temp_data[0], temp_data.size(), sizeof(string), isGreater);
+			for (vector<string>::iterator iterator = temp_data.begin(); iterator < temp_data.end(); ++iterator)
+			{
+				if (iterator != temp_data.begin())
+				{
+					temp_outputStream << endl;
+				}
+				temp_outputStream << *iterator;
+			}
 			temp_outputStream.close();
+			temp_outputStream.clear();
+
+			// create new temporary file, reset buffer_size, reset temp_data to fill again
 			number_temp_file++;
-			buffer_size = 0;
 			filename = "text_temp_" + to_string(number_temp_file) + ".txt";
 			temp_outputStream.open(filename);
-			// get the line
-			temp_outputStream << line;
-			buffer_size += line.size();	
+			buffer_size = 0;
+			temp_data.clear();
+			temp_data.push_back(line);
+			buffer_size += line.size();
 		}
-		is_first_line = (is_first_line == 1) ? 0:0;
+	}
+
+	// sort then store into the last temporary file
+	qsort(&temp_data[0], temp_data.size(), sizeof(string), isGreater);
+	for (vector<string>::iterator iterator = temp_data.begin(); iterator < temp_data.end(); ++iterator)
+	{
+		if (iterator != temp_data.begin())
+		{
+			temp_outputStream << endl;
+		}
+		temp_outputStream << *iterator;
 	}
 	temp_outputStream.close();
 
-	// 2. Sort all temporary files, loop from 1 -> number_temp_file
-	ifstream temp_in_file;			// Temporary output file
-	for (int i = 1; i <= number_temp_file; i++)
-	{
-		// read temporary file
-		filename = "text_temp_" + to_string(i) + ".txt";
-		vector<string> temp_data;	// Vector data of a temporary file
-		temp_in_file.open(filename);
-
-		// get all lines to temp_data, prepare for sorting
-		while(!temp_in_file.eof())
-		{
-			getline(temp_in_file, line);
-			temp_data.push_back(line);
-		}
-		temp_in_file.close();
-
-		// sorting...
-		qsort(&temp_data[0], temp_data.size(), sizeof(string), isGreater);
-		// sort(temp_data.begin(), temp_data.end());			// another option for sorting
-
-		//  write sorted temp_data to temporary file
-		temp_outputStream.open(filename);
-		for (vector<string>::iterator iterator = temp_data.begin(); iterator < temp_data.end(); ++iterator)
-		{
-			temp_outputStream << *iterator << endl;
-			
-		}
-		temp_outputStream.close();
-	}
-
-	// 3. Merge all sorted temporary file to output file
-	ifstream temp_file[number_temp_file];		// array containering all temporary files
-	vector<pair<int, string>> container;		// container for first elements of temporary files
-
-	// initialize the container
+	// *** 2. Merge all sorted temporary file to output file
+	//        Using input buffer, output buffer to avoid bottle necks from IO
+	ifstream temp_file[number_temp_file];
+	
+	// open all temporary files
 	for (int i = 0; i < number_temp_file; i++)
 	{
 		filename = "text_temp_" + to_string(i + 1) + ".txt";
 		temp_file[i].open(filename);
-		getline(temp_file[i], line);
-		container.push_back(pair<int, string>(i, line));
 	}
 
-	// Create outputStream to write an output file
+	// create outputStream to write the output file
 	ofstream outputStream(outputfileName);
 	if(!outputStream.is_open())
 	{
@@ -141,37 +129,118 @@ int main()
 		return 1;
 	}
 
-	pair<int, string> min_line;					// minimum line of container
-	int index;									// min_line's index on container
-	index = min_pair(container, min_line);		// fill index, fill min_line
+	// in, out buffer for IO process
+	queue<string> input_buffer[number_temp_file];
+	queue<string> output_buffer;
+	int input_buffer_max_size = (int) RAM_SIZE / (number_temp_file + 1);                          // input buffer max size: ~ RAMSIZE/(number_temp_file + 1)
+	int output_buffer_max_size = input_buffer_max_size;                                           // output buffer max size: ~ RAMSIZE/(number_temp_file + 1)
+	int input_buffer_size;                                                                        // current input buffer                       
+	int output_buffer_size;                                                                       // curretn output buffer
 
-	// merging all temporary files -> output file
-	is_first_line = 1;
-	while (index >= 0)
+	// first, fill all input buffers whose size ~ input_buffer_max_size
+	for (int i = 0; i < number_temp_file; i++)
 	{
-		if (!is_first_line)
+		input_buffer_size = 0;
+		while(input_buffer_size < output_buffer_max_size && !temp_file[i].eof())
 		{
-			outputStream << endl;
+			getline(temp_file[i], line); 
+			input_buffer[i].push(line);
+			input_buffer_size += line.size();
 		}
-		outputStream << min_line.second;
-		container.erase(container.begin() + index);
-		if (!temp_file[min_line.first].eof())
-		{
-			getline(temp_file[min_line.first], line);
-			if (line.size())
-				container.push_back(pair<int, string>(min_line.first, line));
-		}
-		index = min_pair(container, min_line);
-		is_first_line = (is_first_line == 1) ? 0:0;
 	}
 
-	// remove all temporary file
+	vector<pair<int, string>> container;                                                         // container for first elements of input buffers
+	// initialize the container
+	for (int i = 0; i < number_temp_file; i++)
+	{
+		line = input_buffer[i].front();
+		input_buffer[i].pop();
+		container.push_back(pair<int, string>(i, line));
+	}
+
+	pair<int, string> min_line;                                                                  // minimum line of container
+	int index;// min_line's index on container
+	index = min_pair(container, min_line);                                                       // fill index, fill min_line
+
+	bool is_first_line = true;                                                                   // Check the first line of final output file or NOT
+	
+	output_buffer_size = 0;
+	// while container is not empty, try to find minimum element to put to output buffer
+	// if output buffer is full, pop it to DISK
+	// if container is empty, try to fill it again by input buffer
+	// if input buffer is empty, to to fill it by temporary file
+	while (index >= 0)
+	{
+		// push to output buffer, if output buffer is full, put all to output file
+		output_buffer.push(min_line.second);
+		output_buffer_size += min_line.second.size();
+
+		if (output_buffer_size > output_buffer_max_size)
+		{
+			while(output_buffer.size() > 0)
+			{
+				line = output_buffer.front();
+				output_buffer.pop();
+				if (!is_first_line)
+				{
+					outputStream << endl;
+				}
+				outputStream << line;
+				is_first_line = (is_first_line == true) ? false:false;
+			}
+			output_buffer_size = 0;
+		}
+
+		// remove this element out of the container
+		container.erase(container.begin() + index);
+
+		// if a input buffer is empty, fill it again
+		for (int i = 0; i < number_temp_file; i++)
+		{
+			if (!input_buffer[i].size())
+			{
+				input_buffer_size = 0;
+				while(input_buffer_size < output_buffer_max_size && !temp_file[i].eof())
+				{
+					getline(temp_file[i], line); 
+					input_buffer[i].push(line);
+					input_buffer_size += line.size();
+				}
+			}
+		}
+
+		// if this input buffer is not empty, pop one element to the container
+		if (input_buffer[min_line.first].size())
+		{
+			if (input_buffer[min_line.first].size()){
+				container.push_back(pair<int, string>(min_line.first, input_buffer[min_line.first].front()));
+			}
+			input_buffer[min_line.first].pop();
+		}
+
+		index = min_pair(container, min_line);
+	}
+
+	// put the last output buffer to output file
+	while(output_buffer.size() > 0)
+	{
+		line = output_buffer.front();
+		output_buffer.pop();
+		outputStream << endl;
+		outputStream << line;
+	}
+
+	outputStream.close();
+
+	// remove all temporary files
 	for (int i = 1; i <= number_temp_file; i++)
 	{
 		filename = "text_temp_" + to_string(i) + ".txt";
 		remove(filename.c_str());
 	}
 
+	cout << "input_buffer_max_size: " << input_buffer_max_size << endl;
+	cout << "output_buffer_max_size: " << output_buffer_max_size << endl;
 	cout << "Number of temporary files: " << number_temp_file << endl;
 	return 0;
 }
